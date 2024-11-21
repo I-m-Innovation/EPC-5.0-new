827c97bd2ec171ddf6997c5597e88b53e9437a91
stefanoZS <stefano.trevisan@zilioservice.com>
2024-11-21 16:48:37 +0100

    Debug = False

--- EPC_5_0/settings.py
+++ EPC_5_0/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "EPC_5_0_v02/static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
