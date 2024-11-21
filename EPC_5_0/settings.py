7f1108dfb44334aef900c9289059aa1782f7d34c
stefanoZS <stefano.trevisan@zilioservice.com>
2024-11-21 16:26:26 +0100

    Modifica statici

--- EPC_5_0/settings.py
+++ EPC_5_0/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'EPC_5_0_v02/static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "EPC_5_0_v02/static"
 ]
 
 # Default primary key field type
