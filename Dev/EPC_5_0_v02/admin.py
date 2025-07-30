87b458fc740db3ec5ec9f18cf1c8eb19c9583116
Giulio <giulio.lazzaro@zilioenvironment.com>
2025-07-30 12:35:50 +0200

    Delete Dev directory

--- Dev/EPC_5_0_v02/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from EPC_5_0_v02.models import Offerta
-# Register your models here.
-
-admin.site.register(Offerta)
--- Dev/EPC_5_0_v02/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Epc50V02Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'EPC_5_0_v02'
